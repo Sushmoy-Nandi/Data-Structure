cff1f4a3d67a274fa93f3c51abea86ae2c866c0d
“Sushmoy <learnsushmoysk1091@gmail.com>
2023-09-26 02:00:50 +0600

    add

--- Singly Linked List/make_constructor.cpp
+++ Singly Linked List/make_constructor.cpp
@@ -5,16 +5,17 @@ class Node
     public:
        int val;
        Node* next;
+       Node(int val)
+       {
+        this->val=val;
+        this->next=NULL;
+       }
 };
 int main()
 {
-    Node a,b;
-    a.val=10;
-    b.val=20;
-
+    Node a(10);
+    Node b(20);
     a.next=&b;
-    b.next=NULL;
-
     cout<<a.val<<endl;
     //cout<<b.val<<endl; b.val=a.next
     //derefence
